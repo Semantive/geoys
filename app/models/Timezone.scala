482214f626ed896859cc9494c13ed65fe7e96723
Piotr Jędruszuk <pjedruszuk@semantive.pl>
2013-12-31 04:26:38 +0100

    Proper Timezone definition

--- app/models/Timezone.scala
+++ app/models/Timezone.scala
@@ -1,4 +1,9 @@
 package models
 
-case class Timezone(id: Option[Long], countryId: Long, name: String, gmtOffset: Double, dstOffset: Double, rawOffset: Double)
+case class Timezone(id: Option[Long],
+                    countryId: Long,
+                    name: String,
+                    gmtOffset: Double,
+                    dstOffset: Double,
+                    rawOffset: Double) extends AbstractEntity
 
