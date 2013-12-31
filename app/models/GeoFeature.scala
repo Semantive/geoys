0a8a7d5c68b1f04b9e557b160b269fbf693f1db7
Amadeusz Kosik <akosik@semantive.com>
2013-12-31 17:21:08 +0100

    Administrative division and populated place => GeoFeature

---
+++ app/models/GeoFeature.scala
@@ -0,0 +1,24 @@
+package models
+
+import com.vividsolutions.jts.geom.Point
+
+case class GeoFeature(id: Option[Long],
+                      name: String,
+                      asciiName: String,
+                      fClass: String,
+                      fCode: String,
+                      admCode: Option[String],
+                      timezoneId: Option[Long],
+                      location: Option[Point],
+                      countryId: Long,
+                      population: Option[Long],
+                      geoId: Long,
+                      adm1Id: Option[Long] = None,
+                      adm2Id: Option[Long] = None,
+                      adm3Id: Option[Long] = None,
+                      adm4Id: Option[Long] = None,
+                      parentId: Option[Long] = None,
+                      level: Int = 1) extends AbstractEntity
+
+
+
