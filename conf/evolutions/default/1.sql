bf0d89f57408ce463c2f3e6d51f24bb6b3bcae45
Amadeusz Kosik <eternalsoulharvester@gmail.com>
2014-01-06 21:59:19 +0100

    SQL table creation script.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -1,29 +1,24 @@
 # --- !Ups
 CREATE TABLE continent (
-  id            SERIAL                PRIMARY KEY,
-  code          CHAR(2)               UNIQUE                      NOT NULL,
-  name          VARCHAR(200)                                      NOT NULL,
-  ascii_name    VARCHAR(200)                                      NOT NULL,
-  geo_id        INTEGER               UNIQUE                      NOT NULL
+  geoname_id    INTEGER               PRIMARY KEY,
+  code          CHAR(2)               UNIQUE                      NOT NULL
 );
 
--- Countries
 CREATE TABLE country (
-  id            SERIAL                PRIMARY KEY,
-  iso2          CHAR(2)               UNIQUE                      NOT NULL,
-  iso3          CHAR(3)               UNIQUE                      NOT NULL,
-  fips          CHAR(2),
-  name          VARCHAR(200)                                      NOT NULL,
-  ascii_name    VARCHAR(200)                                      NOT NULL,
-  continent_id  INTEGER               REFERENCES continent(id)    NOT NULL,
-  location      GEOMETRY(Point, 4326),
+  geoname_id    INTEGER               PRIMARY KEY,
+  iso2_code     CHAR(2)               UNIQUE                      NOT NULL,
+  iso3_code     CHAR(3)               UNIQUE                      NOT NULL,
+  iso_numeric   CHAR(3)               UNIQUE                      NOT NULL,
+  fips_code     CHAR(2)               UNIQUE                      NOT NULL,
   population    INTEGER                                           NOT NULL,
-  geo_id        INTEGER               UNIQUE                      NOT NULL
+  continent_id  INTEGER               REFERENCES continent(id)    NOT NULL,
+  tld           CHAR(8)                                           NOT NULL,
+  currency_code CHAR(3)                                           NOT NULL
 );
 
 CREATE TABLE timezone (
   id            SERIAL                PRIMARY KEY,
-  country_id    INTEGER               REFERENCES Country(id)      NOT NULL,
+  country_id    INTEGER               REFERENCES country(id)      NOT NULL,
   name          VARCHAR(40)           UNIQUE                      NOT NULL,
   gmt_offset    NUMERIC(3, 1)                                     NOT NULL,
   dst_offset    NUMERIC(3, 1)                                     NOT NULL,
@@ -31,110 +26,34 @@ CREATE TABLE timezone (
 );
 
 CREATE TABLE feature (
-  id            SERIAL,
-  name          VARCHAR(200)          NOT NULL,
-  ascii_name    VARCHAR(200)          NOT NULL,
-  f_class       CHAR(1)               NOT NULL,
-  f_code        VARCHAR(10)           NOT NULL,
+  geoname_id    INTEGER               PRIMARY KEY,
+  feature_class CHAR(1)                                           NOT NULL,
+  feature_code  VARCHAR(10)                                       NOT NULL,
   adm_code      VARCHAR(40),
-  timezone_id   BIGINT,
-  location      GEOMETRY(Point,4326),
-  country_id    BIGINT                NOT NULL,
+  country_id    INTEGER               REFERENCES feature(geoname_id),
+  adm1_id       INTEGER               REFERENCES feature(geoname_id),
+  adm2_id       INTEGER               REFERENCES feature(geoname_id),
+  adm3_id       INTEGER               REFERENCES feature(geoname_id),
+  adm4_id       INTEGER               REFERENCES feature(geoname_id),
+  parent_id     INTEGER               REFERENCES feature(geoname_id),
+  timezone_id   INTEGER               REFERENCES timezone(id),
   population    BIGINT,
-  geo_id        BIGINT                NOT NULL,
-  adm1_id       BIGINT,
-  adm2_id       BIGINT,
-  adm3_id       BIGINT,
-  adm4_id       BIGINT,
-  parent_id     BIGINT,
-  level         INTEGER
+  wiki_link     VARCHAR(255)
 );
 
-CREATE TABLE adm1 (
-  CHECK(f_class = 'A' AND f_code = 'ADM1')
-) INHERITS(feature);
+CREATE TABLE name_translation (
+  geoname_id    INTEGER               NOT NULL,
+  language      CHAR(8)               NOT NULL,
+  name          VARCHAR(255)          NOT NULL,
 
-ALTER TABLE adm1 ADD CONSTRAINT fk_adm1_country
-  FOREIGN KEY(country_id) REFERENCES country(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE adm1 ADD CONSTRAINT fk_adm1_timezone
-  FOREIGN KEY(timezone_id) REFERENCES timezone(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
+  PRIMARY KEY(geoname_id, language)
+);
 
-CREATE TABLE adm2 (
-  CHECK(f_class = 'A' AND f_code = 'ADM2')
-) INHERITS(feature);
-
-ALTER TABLE adm2 ADD CONSTRAINT fk_adm2_country
-  FOREIGN KEY(country_id) REFERENCES country(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE adm2 ADD CONSTRAINT fk_adm2_timezone
-  FOREIGN KEY(timezone_id) REFERENCES timezone(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-CREATE TABLE adm3 (
-  CHECK(f_class = 'A' AND f_code = 'ADM3')
-) INHERITS(feature);
-
-ALTER TABLE adm3 ADD CONSTRAINT fk_adm3_country
-  FOREIGN KEY(country_id) REFERENCES country(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE adm3 ADD CONSTRAINT fk_adm3_timezone
-  FOREIGN KEY(timezone_id) REFERENCES timezone(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-CREATE TABLE adm4 (
-  CHECK(f_class = 'A' AND f_code = 'ADM4')
-) INHERITS(feature);
-
-ALTER TABLE adm4 ADD CONSTRAINT fk_adm4_country
-  FOREIGN KEY(country_id) REFERENCES country(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE adm4 ADD CONSTRAINT fk_adm4_timezone
-  FOREIGN KEY(timezone_id) REFERENCES timezone(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-CREATE TABLE ppl (
-  CHECK(f_class = 'P')
-) INHERITS(feature);
-
-ALTER TABLE ppl ADD CONSTRAINT fk_ppl_country
-  FOREIGN KEY(country_id) REFERENCES country(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-ALTER TABLE ppl ADD CONSTRAINT fk_ppl_timezone
-  FOREIGN KEY(timezone_id) REFERENCES timezone(id) ON UPDATE NO ACTION ON DELETE NO ACTION;
-
-CREATE OR REPLACE FUNCTION feature_insert_trigger()
-  RETURNS TRIGGER AS $$
-BEGIN
-    IF(NEW.f_class = 'P') THEN
-        INSERT INTO ppl VALUES (NEW.*);
-    ELSIF(NEW.f_class = 'A') THEN
-      IF(NEW.f_code = 'ADM1') THEN
-        INSERT INTO adm1 VALUES (NEW.*);
-      ELSIF(NEW.f_code = 'ADM2') THEN
-        INSERT INTO adm2 VALUES (NEW.*);
-      ELSIF(NEW.f_code = 'ADM3') THEN
-        INSERT INTO adm3 VALUES (NEW.*);
-      ELSIF(NEW.f_code = 'ADM4') THEN
-        INSERT INTO adm4 VALUES (NEW.*);
-      ELSE
-        RAISE EXCEPTION 'Unknown A code. Fix the feature_insert_trigger() function.';
-      END IF;
-    ELSE
-      RAISE EXCEPTION 'Unknown feature class. Fix the feature_insert_trigger() function.';
-    END IF;
-
-    RETURN NULL;
-END;
-$$
-LANGUAGE plpgsql;
-
-CREATE TRIGGER insert_feature_trigger
-  BEFORE INSERT ON feature
-FOR EACH ROW EXECUTE PROCEDURE feature_insert_trigger();
+-- todo: partycje
 
 # --- !Downs
 
-DROP TRIGGER insert_feature_trigger ON feature;
-DROP FUNCTION  feature_insert_trigger();
-
-DROP TABLE ppl;
-DROP TABLE adm4;
-DROP TABLE adm3;
-DROP TABLE adm2;
-DROP TABLE adm1;
+DROP TABLE name_translation;
 DROP TABLE feature;
 DROP TABLE timezone;
 DROP TABLE country;
