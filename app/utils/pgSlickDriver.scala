f0ea49332e2c61848427203e2fff43d0c3515ade
Piotr Jędruszuk <pjedruszuk@semantive.com>
2014-01-24 11:42:36 +0100

    Implicit conversion to Point for SQL queries, example of function definition

--- app/utils/pgSlickDriver.scala
+++ app/utils/pgSlickDriver.scala
@@ -2,6 +2,8 @@ package utils
 
 import slick.driver.PostgresDriver
 import com.github.tminglei.slickpg._
+import scala.slick.jdbc.GetResult
+import com.vividsolutions.jts.geom.Point
 
 /**
  * Driver for slick-pg extension.
@@ -16,7 +18,12 @@ trait pgSlickDriver extends PostgresDriver
   with PostGISSupport {
 
   override val Implicit = new ImplicitsPlus {}
-  override val simple = new SimpleQLPlus {}
+  override val simple = new SimpleQLPlus {
+    implicit val pointConverter = GetResult(r => new GeometryTypeMapper[Point].nextValue(r))
+
+    val st_distance_sphere = SimpleFunction.binary[Point, Point, Double]("ST_DISTANCE_SPHERE")
+  }
+
 
   trait ImplicitsPlus extends Implicits
     with ArrayImplicits
@@ -27,7 +34,13 @@ trait pgSlickDriver extends PostgresDriver
 
   trait SimpleQLPlus extends SimpleQL
     with ImplicitsPlus
-    with SearchAssistants
+    with SearchAssistants {
+
+
+  }
+
+
+
 }
 
 object pgSlickDriver extends pgSlickDriver
